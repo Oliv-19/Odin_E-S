ed7beac2b4b198a77ef6598c01fd403ad02ff8bf
Oliv-19 <betza.bruguera@gmail.com>
2024-05-18 11:55:08 -0400

    Add different grid sizes, only works 32

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
 let divsWrapper = document.querySelector('.divsWrapper')
 
-for (let i = 0; i < 256; i++){
-    let squareDiv = document.createElement('div')
-    squareDiv.className = 'squareDiv'
-    divsWrapper.append(squareDiv)
+let btn = document.querySelectorAll('.btn')
 
-    squareDiv.addEventListener('mouseover', ()=>{
-    squareDiv.style.backgroundColor = 'aqua'
+    //console.log(cWidth , cHeight)
+
+
+
+btn.forEach(element => {
+    let cSize = (element.id * element.id)
+    console.log(element.id)
+
+    element.addEventListener('click', ()=> {
+        
+        for (let i = 0; i < cSize; i++){
+            let squareDiv = document.createElement('div')
+            squareDiv.className = 'squareDiv'
+            divsWrapper.append(squareDiv)
+            
+            if (element.id == 32){
+               // divsWrapper.style.cssText = 'width : 559px; height :550px;';
+                squareDiv.style.cssText = 'width : 17px; height :17px;'
+                
+            }else if (element.id == 64){
+                divsWrapper.style.cssText = 'width : 659px; ';
+                squareDiv.className = 'squareDiv2'
+                //squareDiv.style.cssText = 'width : 17px; height :17px;'
+                
+            }
+
+
+            squareDiv.addEventListener('mouseover', ()=>{
+                squareDiv.style.backgroundColor = 'aqua'
+                
+            
+            })
+        }
+
+
+        
+    })
 })
-}
+
 
