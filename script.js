089c588f240d9f755c8352d36c39b1956bca27f8
Oliv-19 <betza.bruguera@gmail.com>
2024-05-21 09:47:26 -0400

    Change ui

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let divsWrapper = document.querySelector('.divsWrapper')
+let dark = document.querySelector('.dark')
 
 let btn = document.querySelectorAll('.btn')
 let colorInput = document.getElementById('color');
@@ -8,6 +9,11 @@ colorInput.addEventListener('input', () =>{
    color = colorInput.value;
     
 });
+let darkening = false
+dark.addEventListener('click', function(){
+    darkening = true
+})
+
 
 btn.forEach(element => {
     let cSize = (element.id * element.id)
@@ -15,6 +21,7 @@ btn.forEach(element => {
 
     element.addEventListener('click', ()=> {
         removeDivs()
+        darkening = false
         for (let i = 0; i < cSize;i++) {
             let squareDiv = document.createElement('div')
             squareDiv.className = 'squareDiv'
@@ -29,6 +36,12 @@ btn.forEach(element => {
             squareDiv.addEventListener('mouseover', ()=>{
                 
                 squareDiv.style.backgroundColor = color
+
+                if (darkening == true){
+                    let opacity = Number(squareDiv.style.opacity);
+                    squareDiv.style.opacity = opacity >= 1 ? "1" : opacity + 0.1 + "";
+                }
+                
                 
             
             })
